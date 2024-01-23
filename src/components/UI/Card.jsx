4ffb7f9d7ef27c9d8e67928f3811217efcfe895c
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-23 11:40:22 +0200

    car prop validaton

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-function Card({ name, count }) {
+function Card({ name, count, arr }) {
   return (
     <div>
       <p>card</p>
       <h2>{name}</h2>
       <p>{count}</p>
+      {arr?.map((str) => (
+        <p key={str}>one</p>
+      ))}
     </div>
   );
 }
@@ -13,6 +16,7 @@ function Card({ name, count }) {
 Card.propTypes = {
   name: PropTypes.string.isRequired,
   count: PropTypes.number.isRequired,
+  arr: PropTypes.array,
 };
 
 export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,8 @@ export default function HomePage() {
   return (
     <div className='container'>
       <h1>HomePage</h1>
-      <Card name={'This is cards name'} />
+      <Card name={'This is cards name'} arr={['one', 'two', 'three']} count={50} />
+      <Card name={'other card'} count={50} />
       <button className='btn'>Read more</button>
     </div>
   );
