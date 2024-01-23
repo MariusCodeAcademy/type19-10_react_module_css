4685a091cf2f89ce4086d9a291c411f30a60c0de
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-23 10:50:39 +0200

    onClick

--- src/components/UI/btn/Button.jsx
+++ src/components/UI/btn/Button.jsx
@@ -1,7 +1,11 @@
 import css from './Button.module.css';
 
-export default function Button({ children, className = '' }) {
+export default function Button({ children, className = '', onClick = () => {} }) {
   // return <button className={css.btn + ' ' + className}>{children}</button>;
-  return <button className={`${css.btn} ${className}`}>{children}</button>;
+  return (
+    <button onClick={onClick} className={`${css.btn} ${className}`}>
+      {children}
+    </button>
+  );
   // return <button className={[css.btn, className].join(' ')}>{children}</button>;
 }
--- src/components/tours/singleTour/SingleTour.jsx
+++ src/components/tours/singleTour/SingleTour.jsx
@@ -2,14 +2,20 @@ import Button from '../../UI/btn/Button';
 import css from './SingleTour.module.css';
 
 export default function SingleTour() {
+  function sayHi() {
+    console.log('Hi ------');
+  }
+
   return (
     <div className={css.wrap}>
       <img src={'img/newyork.jpg'} alt={'newyork'} />
       <div className={css.infoBlock}>
-        <h3>New York</h3>
+        <h3 onClick={sayHi}>New York</h3>
         <p className={css.date}>Fri 27 Nov 2016</p>
         <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-        <Button className={css.flexStart}>Buy tickets</Button>
+        <Button onClick={sayHi} className={css.flexStart}>
+          Buy tickets
+        </Button>
       </div>
     </div>
   );
