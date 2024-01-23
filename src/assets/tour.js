e3379925b17ead0fa1c53d49055d786bbd511007
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-23 09:37:14 +0200

    tour dates an img

---
+++ src/assets/tour.js
@@ -0,0 +1,17 @@
+const cards = [
+  {
+    town: 'New York',
+    date: 'Fri 27 Nov 2016',
+    img: '/img/newyork.jpg',
+  },
+  {
+    town: 'Parios',
+    date: 'Fri 28 Nov 2016',
+    img: '/img/paris.jpg',
+  },
+  {
+    town: 'San Fran',
+    date: 'Fri 29 Nov 2016',
+    img: '/img/sanfran.jpg',
+  },
+];
